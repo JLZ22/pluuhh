6f277bba6e6c6965b30a710df8b5ef274dda7488
John Zeng <johnzeng878@gmail.com>
2025-01-21 23:50:02 -0500

    delete test

--- src/test.py
+++
@@ -1,8 +0,0 @@
-import dotenv
-import os
-
-if 'DISCORD_TOKEN' in os.environ:
-    del os.environ['DISCORD_TOKEN']
-dotenv.load_dotenv('../.env')
-print(os.getenv('DISCORD_TOKEN'))
-print(os.getenv('TEST'))
