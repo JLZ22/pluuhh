89a836c1e0523230644da5e11f5fb65e3ca3a42a
John Zeng <johnzeng878@gmail.com>
2025-08-04 21:09:01 -0700

    sanitize message before checks

--- src/bot.py
+++ src/bot.py
@@ -14,6 +14,9 @@ dir = os.path.dirname(__file__)
 with open(os.path.join(dir, 'keywords.yaml'), 'r') as f:
     config = yaml.safe_load(f)
     assert isinstance(config, dict), 'keywords.yaml is not a dictionary'
+    
+def sanitize(msg):
+    return re.sub(r'[^a-zA-Z0-9\s]', '', msg.lower())
 
 @client.event
 async def on_ready():
@@ -24,13 +27,19 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    if not re.match(pluh_pattern, message.channel.name.lower()):
+    if not re.search(pluh_pattern, message.channel.name.lower()):
         return
-    
+
+    msg = sanitize(message.content)
+    if not msg:
+        return
     for word, link in config.items():
-        if word in message.content.lower():
-            await message.channel.send(link)
-            break
+        if word in msg:
+            try:
+                await message.channel.send(link)
+            except Exception as e:
+                print(f"Error sending message: {e}")
+            return
 
 if 'DISCORD_TOKEN' in os.environ:
     del os.environ['DISCORD_TOKEN']    
